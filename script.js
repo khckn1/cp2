122307bb814ebe33b1da44f159974003ae793614
khckn1 <khckn1@gmail.com>
2021-03-19 13:41:33 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let map;
+
+function initMap(lat, lon) {
+  if (!lat || !lon) {
+    lat = 40.249232;
+    lon = -111.649238;
+  }
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: lat, lng: lon},
+    zoom: 15,
+  });
+}
+
+function createMap(element) {
+  let loc = element.currentTarget.alt.split(' ');
+  let lat = parseFloat(loc[0]);
+  let lon = parseFloat(loc[1]);
+  initMap(lat, lon);
+}
+
+document.getElementById('picture1').addEventListener('click', createMap);
+document.getElementById('picture2').addEventListener('click', createMap);
+document.getElementById('picture3').addEventListener('click', createMap);
